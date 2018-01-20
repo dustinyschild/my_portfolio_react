d9e308318fe8c327c67e1f213e3cf92bc81f59dd
Dustin <dyelland@gmail.com>
2018-01-19 19:28:12 -0600

    added eslint react plugin globally and changed dist dir to build

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   devtool: 'cheap-module-eval-source-map',
   entry: './src/main.js',
   output: {
-    path: __dirname + '/dist',
+    path: __dirname + '/build',
     filename: 'app.bundle.js',
     publicPath: '/'
   },
