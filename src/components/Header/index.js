5681868dc6c7fccc5664132442cc33c16f807246
Dustinyschild <dyelland@gmail.com>
2018-03-12 18:54:46 -0500

    refactored header to bem and added style import to plop template

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import './_Header.scss';
+import React from 'react';
+
+const Header = () => {
+  return(
+    <header className="main__header">
+      <h1 className="main__header__title">Dustin Yelland</h1>
+    </header>
+  );
+};
+
+export default Header;
