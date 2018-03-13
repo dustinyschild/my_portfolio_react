cc5ee18bbff43d66a3573059a0c92b05df33748d
Dustinyschild <dyelland@gmail.com>
2018-03-12 22:37:48 -0500

    built out nav component and styling

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -1,14 +1,26 @@
 import './_MainNav.scss';
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { ABOUT_ME_ROUTE } from '../constants/routes';
+import { NOT_IMPLEMENTED, ABOUT_ME_ROUTE } from '../constants/routes';
 
 const MainNav = ()  => {
   return (
     <div className='main-nav'>
-      <Link to={ABOUT_ME_ROUTE}>
-        About Me
-      </Link>
+      <div className='main-nav__tab'>
+        <Link to={NOT_IMPLEMENTED}>{/* Change route */}
+          About Me
+        </Link>
+      </div>
+      <div className='main-nav__tab'>
+        <Link to={NOT_IMPLEMENTED}>{/* Change route */}
+          My Work
+        </Link>
+      </div>
+      <div className='main-nav__tab'>
+        <Link to={NOT_IMPLEMENTED}>{/* Change route */}
+          My Home Projects
+        </Link>
+      </div>
     </div>
   );
 };
